5884f081e4a154007f77306b81b7556b20633c0c
paulo-fs <jr.paulo87@gmail.com>
2022-11-23 10:20:03 -0300

    fix: custom email tittle

--- app/Services/sendMail.ts
+++ app/Services/sendMail.ts
@@ -3,6 +3,10 @@ import User from 'App/Models/User'
 
 export async function sendMail(user: User, template: string): Promise<void> {
   await Mail.send((message) => {
-    message.from('TGL').to(user.email).subject('Welcome to TGL').htmlView(template, { user })
+    message
+      .from('TGL')
+      .to(user.email)
+      .subject(`Sendo bem vindo(a) ${user.name}`)
+      .htmlView(template, { user })
   })
 }
